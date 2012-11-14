6c467a34b84ac10b377a2b95ef0a9a2f2d4b1a8d
microcai <microcai@fedoraproject.org>
2012-11-14 22:36:45 +0800

    btr: add free

--- brt/mm.c
+++ brt/mm.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 
 void * brt_operator_new( size_t );
+void brt_ooperator_delete( void*);
 
 void* brt_operator_new( size_t __size)
 {
 	return malloc(__size);
 }
 
+void brt_ooperator_delete(void* __ptr)
+{
+	free(__ptr);
+}
 
