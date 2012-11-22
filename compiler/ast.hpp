0a756ef2aba3acb2fa42046612038f2619cb22e5
Jack <jack.wgm@gmail.com>
2012-11-22 18:15:52 +0800

    add . to comment end, fix stupid vc
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- compiler/ast.hpp
+++ compiler/ast.hpp
@@ -1,4 +1,4 @@
-﻿/*
+/*
     defination of QBASIC Abstruct Syntax Tree
     Copyright (C) 2012  microcai <microcai@fedoraproject.org>
 
@@ -53,19 +53,19 @@ enum MathOperator {
 
 
 enum Linkage {
-	STATIC = 1,	//静态函数，无导出
-	EXTERN,		//导出函数
-	IMPORTC,		//导入C函数，这样就可以使用 C 函数调用了，算是我提供的一个扩展吧
+	STATIC = 1,			// 静态函数，无导出.
+	EXTERN,				// 导出函数.
+	IMPORTC,			// 导入C函数，这样就可以使用 C 函数调用了，算是我提供的一个扩展吧.
 };
 
 enum ReferenceType {
-	BYVALUE,	//传值
-	BYREF,	//引用，实质就是指针了. 函数的默认参数是传引用
+	BYVALUE,		// 传值.
+	BYREF,			// 引用，实质就是指针了. 函数的默认参数是传引用.
 };
 
 class CodeBlockAST;
 class FunctionDimAST;
-// 逐层传递 给各个 virtual 操作, 虽然很大, 但是不传指针, 直接传值
+// 逐层传递 给各个 virtual 操作, 虽然很大, 但是不传指针, 直接传值.
 class ASTContext
 {
 public:
@@ -76,10 +76,10 @@ public:
 		, func(0)
 	{}
 
-	llvm::Function*		llvmfunc; //当前的函数位置
-	CodeBlockAST*		codeblock; //当前的代码块位置, 用于符号表
-	llvm::BasicBlock*	block; //当前的插入位置
-	llvm::Module*		module; //模块
+	llvm::Function*		llvmfunc;	// 当前的函数位置.
+	CodeBlockAST*		codeblock;	// 当前的代码块位置, 用于符号表.
+	llvm::BasicBlock*	block;		// 当前的插入位置.
+	llvm::Module*		module;		// 模块.
 	FunctionDimAST*		func;
 };
 
@@ -94,12 +94,12 @@ private:
 	AST & operator =(const AST &);
 };
 
-//语句有, 声明语句和表达式语句和函数调用语句
+// 语句有, 声明语句和表达式语句和函数调用语句.
 class StatementAST: public AST
 {
 public:
     StatementAST():parent(0){}
-	CodeBlockAST * parent;	// 避免循环引用 :) , 用在查找变量声明的时候用到. 回溯法
+	CodeBlockAST * parent;	// 避免循环引用 :) , 用在查找变量声明的时候用到. 回溯法.
 	std::string	LABEL;		// label , if there is. then we can use goto
 							// must be uniq among function bodys
 	virtual llvm::BasicBlock* Codegen(ASTContext) = 0;
@@ -118,14 +118,14 @@ public:
 
 #include "type.hpp"
 
-//dim 就是定义一个名字,变量或者函数/过程
+// dim 就是定义一个名字,变量或者函数/过程
 class DimAST: public StatementAST
 {
 public:
 	DimAST(const std::string _name, ExprTypeASTPtr _type);
-	//ExprType type; // the type of the expresion
+	// ExprType type; // the type of the expresion
 	std::string		name; //定义的符号的名字.
-	ExprTypeASTPtr	type; //定义的符号的类型. 将在 typetable 获得type的定义
+	ExprTypeASTPtr	type; //定义的符号的类型. 将在 typetable 获得type的定义.
 	virtual llvm::BasicBlock* Codegen(ASTContext) = 0; // generate alloca
 	// gengerate deconstructions , default is don't do that, override on subclass
 	virtual llvm::BasicBlock* valuedegen(ASTContext ctx){ return ctx.block; };
@@ -146,7 +146,7 @@ public:
     llvm::BasicBlock* Codegen(ASTContext);
 };
 
-// 一个表达式构成的空语句, 当然, 最有可能的是函数调用而已
+// 一个表达式构成的空语句, 当然, 最有可能的是函数调用而已.
 class ExprStmtAST : public StatementAST
 {
 	ExprASTPtr		expr;
@@ -155,7 +155,7 @@ public:
     virtual llvm::BasicBlock* Codegen(ASTContext);
 };
 
-// 有调用就有返回, 这个是返回语句
+// 有调用就有返回, 这个是返回语句.
 class ReturnAST : public StatementAST
 {
 	ExprASTPtr			expr;
@@ -166,16 +166,16 @@ public:
 
 // 代码块, 主要用来隔离变量的作用域. 在 BASIC 中, 代码只有2个作用域 : 全局和本地变量
 // 代码块包含有符号表, 符号表由变量声明或者定义语句填充.
-// 执行符号查找的时候就是到这里查找符号表
+// 执行符号查找的时候就是到这里查找符号表.
 class FunctionDimAST;
 class CodeBlockAST : public StatementAST
 {
 public:
 	std::vector<StatementASTPtr>			statements;	
 
-	CodeBlockAST*							parent; // 父作用域
-	std::map<std::string, DimAST*>			symbols; // 符号表, 映射到定义语句,获得定义语句
-//	std::map<std::string, FunctionDimAST*>	functions; // 函数符号表
+	CodeBlockAST*							parent;		// 父作用域.
+	std::map<std::string, DimAST*>			symbols;	// 符号表, 映射到定义语句,获得定义语句.
+//	std::map<std::string, FunctionDimAST*>	functions;	// 函数符号表.
 
     virtual llvm::BasicBlock* Codegen(ASTContext ctx);
 	virtual llvm::BasicBlock* GenLeave(ASTContext);
@@ -191,7 +191,7 @@ public:
 };
 typedef boost::shared_ptr<CodeBlockAST>	CodeBlockASTPtr;
 
-// IF 语句
+// IF 语句.
 class IFStmtAST : public StatementAST
 {
 public:
@@ -202,21 +202,21 @@ public:
     virtual llvm::BasicBlock* Codegen(ASTContext);
 };
 
-// 所有循环语句的基类, while until for 三种循环
+// 所有循环语句的基类, while until for 三种循环.
 class LoopAST : public StatementAST
 {
-	CodeBlockASTPtr	loopbody; // 循环体
+	CodeBlockASTPtr	loopbody; // 循环体.
 public:
 	LoopAST(CodeBlockAST* body) : loopbody(body) {}
-	// 生成循环体
+	// 生成循环体.
     llvm::BasicBlock*		  bodygen(ASTContext);
     virtual llvm::BasicBlock* Codegen(ASTContext) = 0;
 };
 
-// while 语句
+// while 语句.
 class WhileLoopAST : public LoopAST
 {
-	//循环条件
+	//循环条件.
 	ExprASTPtr	condition;
 public:
 	WhileLoopAST(ExprASTPtr _condition, CodeBlockAST* body);
@@ -260,7 +260,7 @@ public:
 typedef boost::shared_ptr<ArgumentDimAST> ArgumentDimASTPtr;
 
 //变量定义也是一个父 codeblock , 这个 codeblock 将成为函数体 codeblock 的父节点.
-//但是和函数体积本身不处在同一个节点,故而在函数内部定义的同名变量将导致参数无法访问,吼吼
+//但是和函数体积本身不处在同一个节点,故而在函数内部定义的同名变量将导致参数无法访问,吼吼.
 class ArgumentDimsAST : public CodeBlockAST
 {
 public:
@@ -269,9 +269,9 @@ public:
 };
 typedef boost::shared_ptr<ArgumentDimsAST> ArgumentDimsASTPtr;
 
-//函数 AST , 有 body 没 body 来区别是不是定义或者声明 .
-//函数即是声明又是代码块, 故而这里使用了双重继承
-// NOTE : 小心使用双重继承
+//函数 AST , 有 body 没 body 来区别是不是定义或者声明.
+//函数即是声明又是代码块, 故而这里使用了双重继承.
+// NOTE : 小心使用双重继承.
 class FunctionDimAST: public DimAST
 {
 	friend class ReturnAST;
@@ -283,12 +283,12 @@ private:
 public:
 	
 	Linkage		linkage; //链接类型。static? extern ?
-	std::list<VariableDimASTPtr> args_type; //checked by CallExpr
-	ArgumentDimsASTPtr	callargs; // 加到 body 这个 codeblock 符号表,这样 body 能访问到
-	CodeBlockASTPtr	body; //函数体
+	std::list<VariableDimASTPtr> args_type; //checked by CallExpr.
+	ArgumentDimsASTPtr	callargs; // 加到 body 这个 codeblock 符号表,这样 body 能访问到.
+	CodeBlockASTPtr	body; //函数体.
 
     FunctionDimAST(const std::string _name, ExprTypeASTPtr, ArgumentDimsAST * callargs = NULL);
-	//如果是声明, 为 dim 生成 llvm::Function * 声明供使用
+	//如果是声明, 为 dim 生成 llvm::Function * 声明供使用.
     virtual llvm::BasicBlock* Codegen(ASTContext);
 
 	//get a pointer to function
@@ -311,7 +311,7 @@ typedef std::list<ExprASTPtr>	FunctionParameterListAST;
 //内建函数语句. PRINT , 为 PRINT 生成特殊的函数调用:)
 ////////////////////////////////////////////////////////////////////////////////
 
-//打印目的地. 默认打印到屏幕
+//打印目的地. 默认打印到屏幕.
 class PrintIntroAST : public ConstNumberExprAST
 {
 public:
